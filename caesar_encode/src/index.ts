5d0bc595179eec354d39be60a3351f76e41d9d0f
Alexander Ponomarenko <brain16383@yandex.ru>
2024-05-21 23:14:43 +0300

    [caesar_encode] Add CLI file

--- caesar_encode/src/index.ts
+++ caesar_encode/src/index.ts
@@ -1,1 +1,23 @@
-console.log('index.ts!');
+import readline from 'node:readline';
+import { caesarEncode } from './caesarEncode';
+import { caesarDecode } from './caesarDecode';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question(`N? `, (nS) => {
+    rl.question(`text? `, (text) => {
+        console.log(`N = ${nS}`);
+        console.log(`text = ${text}`);
+
+        const encoded = caesarEncode(text, parseInt(nS));
+        console.log(`encoded = ${encoded}`);
+
+        const decoded = caesarDecode(encoded, parseInt(nS));
+        console.log(`decoded = ${decoded}`);
+
+        rl.close();
+    });
+});
